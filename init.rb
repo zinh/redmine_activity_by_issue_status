380386b791d6ddfe741992f73280fae6ac0cf122
Vinh Nguyen <xuanvinhct@gmail.com>
2017-07-26 20:21:02 +0700

    temporary disable timelog_helper_patch

--- init.rb
+++ init.rb
@@ -4,10 +4,10 @@ Redmine::Plugin.register :activity_by_issue_status do
   name 'Activity By Issue Status plugin'
   author 'Vinh Nguyen'
   description 'This help filter time_log activity base on issue\'s status'
-  version '0.0.2'
+  version '0.0.3'
   ActionDispatch::Callbacks.to_prepare do
     require 'time_entry_activity_patch'
     require 'issue_status_patch'
-    require 'timelog_helper_patch'
+#    require 'timelog_helper_patch'
   end
 end
